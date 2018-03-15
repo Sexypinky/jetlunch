7a0e7ec840553481c31f60172e2bd46688b2085e
sexypinky <hospitalqqqq@gmail.com>
2018-03-15 17:10:11 +0300

    make it better

--- spec/features/menus_spec.rb
+++ spec/features/menus_spec.rb
@@ -1,25 +1,30 @@
 require 'rails_helper'
 
 context "test" do
+  let(:user) {create :user}
+
  before(:each) do visit dashboard_pages_path
- fill_in "user_password", with: "pass"
- fill_in "user_email", with: "email"
+ fill_in "user_password", with: "qwerty1234"
+ fill_in "user_email", with: "person@example.com"
  click_button "Log in"
  visit dashboard_pages_path
 end
 
 it "right display add position" do
   if Date.today.strftime("%A")=='Thursday'
-  click_link "Thursday"
-  expect(page).to have_content "Add position"
+    click_link "Thursday"
+    expect(page).to have_content "Add position"
   end
 end
 
 it "right order" do
-  click_link "Wednesday"
-  click_link "Banana"
-  click_link "Sosuges"
-  click_link "Vodka"
-  expect(page).to have_content "You'r ordered successfully "
+  if Date.today.strftime("%A")=='Thursday'
+    click_link "Thursday"
+    click_button "Add position"
+    fill_in "menu_dish", with: "Fish"
+    fill_in "menu_price", with: 12
+    fill_in "menu_first", with: 1
+   expect(page).to have_content "Position successfully created"
+  end
 end
- end
+end
