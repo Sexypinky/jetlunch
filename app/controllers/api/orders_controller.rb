a97231ec2b40509df36f05af00ac5e9835b37286
sexypinky <hospitalqqqq@gmail.com>
2018-03-18 08:04:29 +0300

    make it better

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -3,8 +3,12 @@ http_basic_authenticate_with name: ENV['API_LOGIN'], password: ENV['API_PASS']
 
   def index
     @orders=Order.all
-    @orders=@orders.where(weekday: Date.today.strftime("%A"))
-    render json: @orders
+    if @orders.blank?
+      redirect_to root_path, notice: "Orders is empty"
+    else
+      @orders=@orders.where(weekday: Date.today.strftime("%A"))
+      render json: @orders
+    end
   end
 
 end
